743977050f838f3429379a2de59cd34a02b092e2
User <User@DEV-VM>
2023-03-07 20:44:38 +0100

    20:45

--- Swd.PlayCollector.Gui.Wpf/fMain.xaml.cs
+++ Swd.PlayCollector.Gui.Wpf/fMain.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Swd.PlayCollector.Gui.Wpf.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Ribbon;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -17,11 +19,13 @@ namespace Swd.PlayCollector.Gui.Wpf
     /// <summary>
     /// Interaction logic for fMain.xaml
     /// </summary>
-    public partial class fMain : Window
+    public partial class fMain : RibbonWindow
     {
+        public object AuxiliaryPaneContent { get; set; }
         public fMain()
         {
             InitializeComponent();
+            this.DataContext = new fMainViewModel();
         }
     }
 }
